2fbc264f3ed5ac1ca3c793f168c9817172131ddf
Alan R. Soares <alanrsoars@gmail.com>
2026-01-23 12:32:16 +1300

    chore: add tsdoc

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,10 @@
-// Linear interpolation function
+/**
+ * Linear interpolation function
+ * 
+ * @param start - The starting value
+ * @param end - The ending value
+ * @param t - The interpolation factor
+ * @returns The interpolated value
+ */
 export const lerp = (start: number, end: number, t: number) =>
-	start * (1 - t) + end * t;
+    start * (1 - t) + end * t;
